cddbe4a37ee13b45765a3bae76f9775a7d39470c
mlenorton <32277656+mlenorton@users.noreply.github.com>
2018-11-27 16:39:15 -0800

    Updated year selection
    Allows you to use a start and end year ("yearmin" and "yearmax") to run the SOM, as long as the input files span at least this range.  Also improved naming for saving codes and maps.

--- runSOM_xyf_super.R
+++ runSOM_xyf_super.R
@@ -16,36 +16,42 @@ setwd('/Users/emilynorton/Documents/FATE_Hunsicker_Bond_OcnAtmCoupling/RScripts/
 #option to set seed for reproducible results
 set.seed(9)   
 
-# Set number of variables we're going to compare, and their grid sizes, "SLP"/"HGT" (both same grid) or "SKT" (if strictly timeseries var, it doesn't matter which one)
-compsize <- 3   #1 = univariable "som", 2 = two co-variable "xyf", 3 or more = multivariable "supersom"
-
-vargrid1 <- "HGT"
-vargrid2 <- "SKT" 
-vargrid3 <- "SLP"
-vargrid4 <- "NA"
+# Set number of variables we're going to compare 
+compsize <- 4   #1 = univariable "som", 2 = two co-variable "xyf", 3 or more = multivariable "supersom"
 
 #set variable filenames to load - only has data for the sea points
-#file1 <- '../BiologyData/goa.biology.pca.1965.2012.csv'
-#file2 <- '../PhysicalData/WinterAvg_1965to2012_skt.csv'
-file1 <- '../PhysicalData/MonthlyAverages/MonthlyAvg11to3_forYears1948to2018_hgt.csv'
-file2 <- '../PhysicalData/MonthlyAverages/MonthlyAvg11to3_forYears1948to2018_skt.csv'
-file3 <- '../PhysicalData/MonthlyAverages/MonthlyAvg11to1_forYears1948to2018_slp.csv'
-file4 <- "NA"
+#NOTE: these files must contain headers and 'year' column, and must *at least* contain the temporal range of interest
+#file1 <- '../BiologyData/goa.biology.pca.1965.2012.csv'        # Contains V1 and V2
+file1 <- '../BiologyData/goa.biology.pca.1965.2012_V1only.csv'  # Contains V1 only
+#file1 <- '../PhysicalData/MonthlyAverages/MonthlyAvg11to3_forYears1948to2018_hgt.csv'     # Geopotential height at 200mbar (hPa)
+file2 <- '../PhysicalData/MonthlyAverages/MonthlyAvg11to3_forYears1948to2018_skt.csv'     # Skin Temp
+file3 <- '../PhysicalData/MonthlyAverages/MonthlyAvg11to1_forYears1948to2018_slp.csv'     # Sea Level Pressure
+#file4 <- '../PhysicalData/pdo_1900to2018_FebtoApr_avg.csv'     # PDO averaged Feb to Apr
+file4 <- '../PhysicalData/npgo_1950to2018_FebtoApr_avg.csv'     # NPGO averaged Feb to Apr
+
+# Set variable grid sizes: "SLP"/"HGT" (both same grid) or "SKT". If strictly timeseries var (e.g. biol V1), it doesn't matter what you choose - the slp grid will be used - so just use the variable name
+vargrid1 <- "biolV1"
+vargrid2 <- "SKT" 
+vargrid3 <- "SLP"
+vargrid4 <- "npgo"
+
+# Set temporal range of interest  **THIS IS NEW!! And allows you to re-use input files with longer temporal ranges
+yearmin <- 1965
+yearmax <- 2012
 
 #set dimensions for the som grid (2D) and shape of somgrid (options: 'hexagonal','rectangular')
 sdim1 <- 3 
 sdim2 <- 2
-
 sshape <- 'hexagonal' 
 
-# set relative weights for each variable (up to 4)
+# set relative weights for each variable (up to 4 variables)
 wei1 <- 1   #weight for var 1
 wei2 <- 1   #weight for var 2
 wei3 <- 1   #weight for var 3
-wei4 <- NA  #weight for var 4
+wei4 <- 1   #weight for var 4
 
 
-#Which types of plots do you want to make? 
+# Which types of plots do you want to make? 
 booCodes <- 'T'
 booQuality <- 'T'
 booCounts <- 'T'
@@ -57,17 +63,17 @@ title_name <- "Test superSOM of HGT, SKT, SLP for 1948-2018"
 plot_fname <- "test_plots"
 
 #Do you want to save SOM codes or maps as .csv files?
-saveCodes <- 'F'
-saveMaps <- 'F'
+saveCodes <- 'T'
+saveMaps <- 'T'
 
-# Code and map filenames
-codes_fname <- 'codes_TEST.csv'
-codes1_fname <- 'codes2_SOM2_xyf_biolV1andV2_withSKT_1965to2012.csv'
-codes2_fname <- 'codes2_SOM2_xyf_SKT_withbiolV1andV2_1965to2012.csv'
-codes3_fname <- 'codes2_superSOM_SLP_test.csv'
-codes4_fname <- 'codes2_superSOM_VAR_test.csv'
+# Code and map filenames  ###COME BACK TO THIS>>>need to save depending on number of vars, etc.
+codes1_fname <- sprintf("codes_SOM_%iby%i%sgrid_%ito%iyears_%s.csv", sdim1, sdim2, sshape, yearmin, yearmax, vargrid1)
+codes2_fname <- sprintf("codes_SOM_%iby%i%sgrid_%ito%iyears_%s.csv", sdim1, sdim2, sshape, yearmin, yearmax, vargrid2)
+codes3_fname <- sprintf("codes_SOM_%iby%i%sgrid_%ito%iyears_%s.csv", sdim1, sdim2, sshape, yearmin, yearmax, vargrid3)
+codes4_fname <- sprintf("codes_SOM_%iby%i%sgrid_%ito%iyears_%s.csv", sdim1, sdim2, sshape, yearmin, yearmax, vargrid4)
 
-map_fname = 'maps_unitclassif_SOM2_xyf_SKT_biolV1_v2_1965to2012.csv'
+varnames <- paste(c(vargrid1, vargrid2, vargrid3, vargrid4),collapse="_")
+map_fname = sprintf("maps_unitclassif_SOM_%iby%i%sgrid_%ito%iyears_%s.csv", sdim1, sdim2, sshape, yearmin, yearmax, varnames)
 
 
 ##----------Shouldn't need to change too much below this line ----------------
@@ -82,23 +88,27 @@ library(kohonen)  #for SOMs
 
 data1 <- read_csv(file1)
 data1_forSOM <- data1 %>%   #get rid of year column...may want to prepare in other ways, e.g. make sure all of the same years are used for both files, etc.
+  filter(year <= yearmax, year >= yearmin) %>%
   select(-year)
 
 if (compsize > 1) {
   data2 <- read_csv(file2)
   data2_forSOM <- data2 %>% 
-  select(-year)
+    filter(year <= yearmax, year >= yearmin) %>%
+    select(-year)
 } 
 
 if (compsize > 2){
   data3 <- read_csv(file3)
   data3_forSOM <- data3 %>% 
+    filter(year <= yearmax, year >= yearmin) %>%
     select(-year)
 }
 
 if (compsize > 3){
   data4 <- read_csv(file4)
-  data4_forSOM <- data3 %>% 
+  data4_forSOM <- data4 %>% 
+    filter(year <= yearmax, year >= yearmin) %>%
     select(-year)
 }
 
@@ -122,49 +132,49 @@ sktlonvec <-as.matrix(read.csv(sktlonvecfile, header=FALSE))
 
 # Load grid files in as matrices, and the indices ('seainds') where the data are from - NOTE: 'SLP' (sea level pressure)
 # and 'HGT' (geopotential height at 200mbar) have the same grid and sea indices
-if (vargrid1 == 'SLP' | vargrid1 == 'HGT') {
-seaindsM1 <- slpseainds
-latvecM1 <- slplatvec
-lonvecM1 <- slplonvec
-} else if (vargrid1 == "SKT") {
+if (vargrid1 == 'SKT') {
 seaindsM1 <- sktseainds
 latvecM1 <- sktlatvec
 lonvecM1 <- sktlonvec
+} else {                       # if vargrid 1 == "SLP" | vargrid2 == 'HGT' (or anything else, assume slp-sized grid)
+seaindsM1 <- slpseainds
+latvecM1 <- slplatvec
+lonvecM1 <- slplonvec
 }
 
 if (compsize > 1) {
-  if (vargrid2 == 'SLP' | vargrid2 == 'HGT') {
-    seaindsM2 <- slpseainds
-    latvecM2 <- slplatvec
-    lonvecM2 <- slplonvec
-  } else if (vargrid2 == "SKT") {
+  if (vargrid2 == 'SKT') {
     seaindsM2 <- sktseainds
     latvecM2 <- sktlatvec
     lonvecM2 <- sktlonvec
+  } else {
+    seaindsM2 <- slpseainds
+    latvecM2 <- slplatvec
+    lonvecM2 <- slplonvec
   }
 }
 
 if (compsize > 2) {
-  if (vargrid3 == 'SLP'| vargrid3 == 'HGT') {
-    seaindsM3 <- slpseainds
-    latvecM3 <- slplatvec
-    lonvecM3 <- slplonvec
-  } else if (vargrid3 == "SKT") {
+  if (vargrid3 == 'SKT') {
     seaindsM3 <- sktseainds
     latvecM3 <- sktlatvec
     lonvecM3 <- sktlonvec
+  } else {
+    seaindsM3 <- slpseainds
+    latvecM3 <- slplatvec
+    lonvecM3 <- slplonvec
   }
 }
 
 if (compsize > 3) {
-  if (vargrid4 == 'SLP'| vargrid4 == 'HGT') {
-    seaindsM4 <- slpseainds
-    latvecM4 <- slplatvec
-    lonvecM4 <- slplonvec
-  } else if (vargrid4 == "SKT") {
+  if (vargrid4 == 'SKT') {
     seaindsM4 <- sktseainds
     latvecM4 <- sktlatvec
     lonvecM4 <- sktlonvec
+  } else {
+    seaindsM4 <- slpseainds
+    latvecM4 <- slplatvec
+    lonvecM4 <- slplonvec
   }
 }
 
@@ -197,9 +207,13 @@ maps_out <- map(som_out)
 
 # Save codes and maps, if desired
 if (saveCodes == 'T') {
-  write.table(codes_out, file = codes_fname, row.names = FALSE, col.names = FALSE, sep = ',')
-  #write.table(codes_out1, file = codes1_fname, row.names=FALSE,col.names=FALSE,sep=',')
-#write.table(codes_out2, file = codes2_fname, row.names=FALSE,col.names=FALSE,sep=',')
+  write.table(codes_out1, file = codes1_fname, row.names=FALSE,col.names=FALSE,sep=',')
+    if (compsize > 1) {
+      write.table(codes_out2, file = codes2_fname, row.names=FALSE,col.names=FALSE,sep=',') }
+    if (compsize > 2) {
+      write.table(codes_out3, file = codes3_fname, row.names=FALSE,col.names=FALSE,sep=',') }
+    if (compsize > 3) {
+      write.table(codes_out4, file = codes4_fname, row.names=FALSE,col.names=FALSE,sep=',') }
 }
 
 if (saveMaps == 'T') {
