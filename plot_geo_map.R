fddfdf932f9f74403f13f1502c26738a1a41833c
mlenorton <32277656+mlenorton@users.noreply.github.com>
2018-12-13 12:27:30 -0800

    Files added via upload
    Input .csv files have been re-shaped so now we're reading first across latitudes (from low to high, i.e. equator to pole) for a given longitude, and then moving across longitudes.  This does not impact the output of the SOM, but the red squiggly code plots do look different - no need to be alarmed.  Mixed layer depth (MLD) and u/v flux (UFLX, VFLX) data have been prepared as input files for the full domain (MLD = 40 - 65N, 170-235E; U/VFLX: 20-68N, 130-250E) and for partial domain (40-65N, 170-235E).  Corresponding grids have been made to allow geospatial plotting of the code results for these variables.  [Note that MLD is available only from ORAS3 from 1959 - 2011, while U/VFLX is available from NCEP from 1948 -2018.]  SKT + SLP input data files have also been changed so that their latitude vector is ascending, which makes plotting easier and consistent with the other variables.  We can now run SOMs on the full domain or any partial domain of choice, as long as the input files are prepared accordingly - this is done in Matlab with "Average_PickYourMonths_SpatialBounds.m'.  Geospatial mapping has been improved so that more maps can fit on the same page, which is handy for running SOMs with more nodes; plot titles have also been improved. I would still like to re-order the plots to match the automated Kohonen plots - this is still on my to-do list.

--- plot_geo_map.R
+++ plot_geo_map.R
@@ -24,49 +24,74 @@ if (compsize == 4) {
   codes_out4 <- codes_out[[4]]
 }
 
-# Make plots for each variable (if the variable isn't spatially explicit, this won't make sense)
+# Make plots for each variable (if the variable isn't spatially explicit, this won't make sense, but that's okay) - try later with ggplot??
+zmax <- max(abs(min(codes_out1)),max(codes_out1))
+zmin <- -zmax
 dev.new()
-par(mfrow=c(sdim2,sdim1))
+#par(mfrow=c(sdim2,sdim1))
+par(mfrow=c(sdim2,sdim1),
+    oma = c(5,4,0,0) + 0.1,
+    mar = c(0,0,1,1) + 0.1)
   for (node in c(1:nodes)){
-    dummy <- matrix(data=NA,nrow=length(lonvecM1),ncol=length(latvecM1))
-    dummy[seaindsM1]<-codes_out1[node,]
-    filled.contour3(x=lonvecM1, y=rev(latvecM1), dummy, xlim=c(120,240), ylim=c(60,20),  zlim=c(-2.5,2.5), nlevels=10, col = rev(brewer.pal(n=10,name='RdBu')), main=paste('Var 1, node', node))  #this works, but doesn't mask out the land
+#    dummy <- matrix(data=NA,nrow=length(lonvecM1),ncol=length(latvecM1))
+    dummyT <- matrix(data=NA,nrow=length(latvecM1),ncol=length(lonvecM1))
+    dummyT[seaindsM1]<-codes_out1[node,]
+    dummy <- t(dummyT)
+          filled.contour3(x=lonvecM1, y=latvecM1, dummy, xlim=c(lonmin,lonmax), ylim=c(latmin,latmax), zlim=c(zmin,zmax), nlevels=10, col = rev(brewer.pal(n=10,name='RdBu')), main=paste(sprintf('%s', grids[1]), 'node', node))  #this works, but doesn't mask out the land
   }
 par(new = "TRUE",plt = c(0.85,0.9,0.25,0.85),las = 1,cex.axis = 1)
-filled.legend(x=lonvecM1, y=rev(latvecM1), dummy, xlim=c(120,240), ylim=c(60,20),  zlim=c(-2.5,2.5), nlevels=10, col = rev(brewer.pal(n=10,name='RdBu'))) 
-
+filled.legend(x=lonvecM1, y=latvecM1[order(latvecM1)], dummy, xlim=c(lonmin,lonmax), ylim=c(latmax,latmin),  zlim=c(zmin,zmax), nlevels=10, col = rev(brewer.pal(n=10,name='RdBu'))) 
 
 if (compsize > 1) {
+  zmax <- max(abs(min(codes_out2)),max(codes_out2))
+  zmin <- -zmax
 dev.new()
-par(mfrow=c(sdim2,sdim1))
+#par(mfrow=c(sdim2,sdim1))
+par(mfrow=c(sdim2,sdim1),
+    oma = c(5,4,0,0) + 0.1,
+    mar = c(0,0,1,1) + 0.1)
   for (node in c(1:nodes)){
-    dummy <- matrix(data=NA,nrow=length(lonvecM2),ncol=length(latvecM2))
-    dummy[seaindsM2]<-codes_out2[node,]
-    filled.contour3(x=lonvecM2, y=rev(latvecM2), dummy, xlim=c(120,240), ylim=c(60,20), zlim=c(-2.5,2.5), nlevels=10, col = rev(brewer.pal(n=10,name='RdBu')), main=paste('Var 2, node', node))  #this works, but doesn't mask out the land
+    dummyT <- matrix(data=NA,nrow=length(latvecM2),ncol=length(lonvecM2))
+    dummyT[seaindsM2]<-codes_out2[node,]
+    dummy <- t(dummyT)
+      filled.contour3(x=lonvecM2, y=latvecM2, dummy, xlim=c(lonmin,lonmax), ylim=c(latmin,latmax), zlim=c(zmin,zmax), nlevels=10, col = rev(brewer.pal(n=10,name='RdBu')), main=paste(sprintf('%s', grids[2]), 'node', node))  #this works, but doesn't mask out the land
   }
 par(new = "TRUE",plt = c(0.85,0.9,0.25,0.85),las = 1,cex.axis = 1)
-filled.legend(x=lonvecM2, y=rev(latvecM2), dummy, xlim=c(120,240), ylim=c(60,20), zlim=c(-2.5,2.5), nlevels=10, col = rev(brewer.pal(n=10,name='RdBu')))  
+filled.legend(x=lonvecM2, y=latvecM2[order(latvecM2)], dummy, xlim=c(lonmin,lonmax), ylim=c(latmax,latmin), zlim=c(zmin,zmax), nlevels=10, col = rev(brewer.pal(n=10,name='RdBu')))  
 }
 
 if (compsize > 2) {
+  zmax <- max(abs(min(codes_out3)),max(codes_out3))
+  zmin <- -zmax
   dev.new()
-  par(mfrow=c(sdim2,sdim1))
+  #par(mfrow=c(sdim2,sdim1))
+  par(mfrow=c(sdim2,sdim1),
+      oma = c(5,4,0,0) + 0.1,
+      mar = c(0,0,1,1) + 0.1)
   for (node in c(1:nodes)){
-    dummy <- matrix(data=NA,nrow=length(lonvecM3),ncol=length(latvecM3))
-    dummy[seaindsM3]<-codes_out3[node,]
-    filled.contour3(x=lonvecM3, y=rev(latvecM3), dummy, xlim=c(120,240), ylim=c(60,20), zlim=c(-2.5,2.5), nlevels=10, col = rev(brewer.pal(n=10,name='RdBu')), main=paste('Var 3, node', node))  #this works, but doesn't mask out the land
+    dummyT <- matrix(data=NA,nrow=length(latvecM3),ncol=length(lonvecM3))
+    dummyT[seaindsM3]<-codes_out3[node,]
+    dummy <- t(dummyT)
+      filled.contour3(x=lonvecM3, y=latvecM3, dummy, xlim=c(lonmin,lonmax), ylim=c(latmin,latmax), zlim=c(zmin,zmax), nlevels=10, col = rev(brewer.pal(n=10,name='RdBu')), main=paste(sprintf('%s', grids[3]), 'node', node))  #this works, but doesn't mask out the land
   }
   par(new = "TRUE",plt = c(0.85,0.9,0.25,0.85),las = 1,cex.axis = 1)
-  filled.legend(x=lonvecM3, y=rev(latvecM3), dummy, xlim=c(120,240), ylim=c(60,20), zlim=c(-2.5,2.5), nlevels=10, col = rev(brewer.pal(n=10,name='RdBu')))  
-}
+  filled.legend(x=lonvecM3, y=latvecM3[order(latvecM3)], dummy, xlim=c(lonmin,lonmax), ylim=c(latmax,latmin), zlim=c(zmin,zmax), nlevels=10, col = rev(brewer.pal(n=10,name='RdBu')))  
+  }
+  
 if (compsize > 3) {
+  zmax <- max(abs(min(codes_out4)),max(codes_out4))
+  zmin <- -zmax
   dev.new()
-  par(mfrow=c(sdim2,sdim1))
+  #par(mfrow=c(sdim2,sdim1))
+  par(mfrow=c(sdim2,sdim1),
+      oma = c(5,4,0,0) + 0.1,
+      mar = c(0,0,1,1) + 0.1)
   for (node in c(1:nodes)){
-    dummy <- matrix(data=NA,nrow=length(lonvecM4),ncol=length(latvecM4))
-    dummy[seaindsM4]<-codes_out4[node,]
-    filled.contour3(x=lonvecM4, y=rev(latvecM4), dummy, xlim=c(120,240), ylim=c(60,20), zlim=c(-2.5,2.5), nlevels=10, col = rev(brewer.pal(n=10,name='RdBu')), main=paste('Var 4, node', node))  #this works, but doesn't mask out the land
+    dummyT <- matrix(data=NA,nrow=length(latvecM4),ncol=length(lonvecM4))
+    dummyT[seaindsM4]<-codes_out4[node,]
+    dummy <- t(dummyT)
+      filled.contour3(x=lonvecM4, y=latvecM4, dummy, xlim=c(lonmin,lonmax), ylim=c(latmin,latmax), zlim=c(zmin,zmax), nlevels=10, col = rev(brewer.pal(n=10,name='RdBu')), main=paste(sprintf('%s', grids[4]), 'node', node))  #this works, but doesn't mask out the land
   }
   par(new = "TRUE",plt = c(0.85,0.9,0.25,0.85),las = 1,cex.axis = 1)
-  filled.legend(x=lonvecM4, y=rev(latvecM4), dummy, xlim=c(120,240), ylim=c(60,20), zlim=c(-2.5,2.5), nlevels=10, col = rev(brewer.pal(n=10,name='RdBu')))  
+  filled.legend(x=lonvecM4, y=latvecM4[order(latvecM4)], dummy, xlim=c(lonmin,lonmax), ylim=c(latmax,latmin), zlim=c(zmin,zmax), nlevels=10, col = rev(brewer.pal(n=10,name='RdBu')))  
 }
